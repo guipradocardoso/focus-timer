57ad72917459d8b099fb3bda07dfd9dcb69b62fa
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-19 06:18:58 -0300

    structuring the countdown

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,5 +1,16 @@
 import state from "./state.js"
 import * as el from "./elements.js"
+
+export function countDown() {
+  if (!state.isRunning) {
+    return
+  }
+  console.log("iniciou")
+
+  // callback function
+  setTimeout(() => countDown(), 1000)
+}
+
 export function updateDisplay(minutes, seconds) {
   // nulish coalesing operator
   minutes = minutes ?? state.minutes
@@ -7,6 +18,4 @@ export function updateDisplay(minutes, seconds) {
 
   el.minutes.textContent = String(minutes).padStart(2, "0")
   el.seconds.textContent = String(seconds).padStart(2, "0")
-
-  EventSource.registerControls()
 }
