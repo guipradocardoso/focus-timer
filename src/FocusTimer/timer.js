60183f2b032e613325b7b7d8783bbcad9fb41ca6
guipradocardoso <guilhermeprado-09@hotmail.com>
2023-09-19 20:18:59 -0300

    countdown logic

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,5 +1,6 @@
 import state from "./state.js"
 import * as el from "./elements.js"
+import { reset } from "./actions.js"
 
 export function countDown() {
   if (!state.isRunning) {
@@ -7,7 +8,23 @@ export function countDown() {
   }
   console.log("iniciou")
 
-  // callback function
+  let minutes = Number(el.minutes.textContent)
+  let seconds = Number(el.seconds.textContent)
+
+  seconds--
+
+  if (seconds < 0) {
+    seconds = 59
+    minutes--
+  }
+  if (minutes < 0) {
+    reset()
+    return
+  }
+
+  updateDisplay(minutes, seconds)
+
+  // callback é uma função chamando outra função function // recursão quando uma função chama ela mesmo dentro
   setTimeout(() => countDown(), 1000)
 }
 
